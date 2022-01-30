cd2df8e8d098774a02e25cfc40f60e9383531356
David Yan <idontwanttotalk@gmail.com>
2022-01-30 16:44:04 -0500

    Init commit

---
+++ src/main/java/com/example/reactspringmysql/ReactSpringMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactspringmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSpringMysqlApplication.class, args);
+    }
+
+}
